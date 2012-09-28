54361e4c79e1282253b0c1f28e0e06d785b5cf14
Hemant Singal <hemants.iitk@gmail.com>
2012-09-28 22:31:19 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommunityNoticeboard::Application.config.secret_token = 'e7e31a24efecc6c3dd14b3861b6c36e092100a0587f6e8340f6b58bd51d7cea27069a7f94fc9e9d0ddc9a8634e1853a3140e58e9edba62ddc905d6a49a245dd0'
