19a8f6546e63caa4142361e46cd907e4b5c4eb92
Hemant Singal <hemants.iitk@gmail.com>
2012-09-29 13:04:34 +0530

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 CommunityNoticeboard::Application.routes.draw do
+  devise_for :users
+
   resources :communities
 
   # The priority is based upon order of creation:
@@ -50,7 +52,12 @@ CommunityNoticeboard::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  authenticated :user do
+    root to: 'communities#index'
+  end
+   
+  root :to => redirect("/users/sign_in")
+
 
   # See how all your routes lay out with "rake routes"
 
